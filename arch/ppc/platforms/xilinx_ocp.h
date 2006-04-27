ecbda58d0e51cf5545d443261c5ceecf41d02001
Stephen Williams <steve@wing.icarus.com>
2006-04-27 10:16:24 -0700

    Xilinx OCB based SysACE driver

---
+++ arch/ppc/platforms/xilinx_ocp.h
@@ -0,0 +1,23 @@
+/*
+ * xilinx_ocp.h
+ *
+ * Include file that defines the Xilinx On Chip Peripherals
+ *
+ * Author: MontaVista Software, Inc.
+ *         source@mvista.com
+ *
+ * 2002 (c) MontaVista, Software, Inc.  This file is licensed under the terms
+ * of the GNU General Public License version 2.  This program is licensed
+ * "as is" without any warranty of any kind, whether express or implied.
+ */
+
+#ifdef __KERNEL__
+#ifndef __ASM_XILINX_OCP_H__
+#define __ASM_XILINX_OCP_H__
+
+#ifndef __ASSEMBLY__
+#include <linux/types.h>
+#endif				/* __ASSEMBLY__ */
+
+#endif				/* __ASM_XILINX_OCP_H__ */
+#endif				/* __KERNEL__ */
---
+++ arch/ppc/platforms/xilinx_ocp/xparameters.h
@@ -0,0 +1,22 @@
+/*
+ * xparameters.h
+ *
+ * This file includes the correct xparameters.h for the CONFIG'ed board
+ *
+ * Author: MontaVista Software, Inc.
+ *         source@mvista.com
+ *
+ * 2002 (c) MontaVista, Software, Inc.  This file is licensed under the terms
+ * of the GNU General Public License version 2.  This program is licensed
+ * "as is" without any warranty of any kind, whether express or implied.
+ */
+
+#include <linux/config.h>
+
+#if defined(CONFIG_XILINX_ML300)
+#include "xparameters_ml300.h"
+#endif
+
+#if defined(CONFIG_MEMEC_2VPX)
+#include "xparameters_2vpx.h"
+#endif
